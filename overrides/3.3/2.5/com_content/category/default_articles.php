bfa09de478a9f9bd75306e4c0c6da45067836cc6
Chris Vaughan <chris@cevsystems.co.uk>
2022-08-05 09:52:08 +0100

    Ramblers fix to list articles in category for PHP 8

--- overrides/3.3/2.5/com_content/category/default_articles.php
+++ overrides/3.3/2.5/com_content/category/default_articles.php
@@ -14,11 +14,14 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('behavior.framework');
 
 // Create some shortcuts.
-$params		= &$this->item->params;
+//  following line sometimes causing an error RAMBLERS
+//$params		= &$this->item->params;
+$params2		= $this->items->params;
+$params=&$params2;
+
 $n			= count($this->items);
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
-
 // Check for at least one editable article
 $isEditable = false;
 
